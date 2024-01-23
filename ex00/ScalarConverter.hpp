c544ddd4acab2f99a1f737119bf859776c640e09
giovanniAfro <giovanni.cacciacavanna@gmail.com>
2024-01-23 12:06:50 +0100

    un parto dio bubu

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: gcavanna <gcavanna@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 19:04:51 by gcavanna          #+#    #+#             */
-/*   Updated: 2024/01/23 00:16:06 by gcavanna         ###   ########.fr       */
+/*   Updated: 2024/01/23 11:40:54 by gcavanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,9 @@ class ScalarConverter
         ~ScalarConverter();
 
         ScalarConverter &operator=(const ScalarConverter &other);
+        static int stoi(const std::string& str, size_t* pos);
+        static double stod(const std::string& str, size_t* pos);
+
     public:
         static void convert(const std::string &literal);
 };  
